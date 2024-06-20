75705ef9f08f81f94685b572f8d13e6365da0848
Sakarr <ctha.sakar@gmail.com>
2024-06-20 20:56:45 +0545

    Add - localstorage access, validation and stylings

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import { useState } from "react";
 import Contact from "./components/Contact";
 import ContactAdder from "./components/ContactAdder";
+import NavBar from "./components/NavBar";
 import "./styles/style.css";
 
 const App = () => {
-  const [contacts, setContacts] = useState([]);
+  const getContacts = JSON.parse(localStorage.getItem("contacts"));
+
+  const [contacts, setContacts] = useState(getContacts ? getContacts : []);
 
   // Child to parent communication
   const addContactData = (contactData) => {
-    setContacts([contactData, ...contacts]);
+    const allContacts = [contactData, ...contacts];
+    setContacts(allContacts);
+    localStorage.setItem("contacts", JSON.stringify(allContacts));
+  };
+
+  const clearAllContacts = () => {
+    localStorage.removeItem("contacts");
+    setContacts([]);
   };
 
   // const childFunctionHandler = () => {
@@ -17,6 +27,7 @@ const App = () => {
 
   return (
     <>
+      <NavBar />
       <div className="contact_adder">
         <ContactAdder
           onContactAdded={addContactData}
@@ -25,9 +36,13 @@ const App = () => {
       </div>
       <div className="contact_list">
         <h3>Contact list:</h3>
+        {/* Unique must be passed for multiple components render */}
         {contacts.map((contact) => (
-          <Contact data={contact}></Contact>
+          <Contact key={contact.id} data={contact}></Contact>
         ))}
+        <button onClick={clearAllContacts} style={{ background: "#ff1919" }}>
+          Clear All Contacts
+        </button>
       </div>
     </>
   );
--- src/components/ContactAdder.js
+++ src/components/ContactAdder.js
@@ -5,9 +5,28 @@ const ContactAdder = (props) => {
   const [name, setName] = useState("");
   const [mobile, setMobile] = useState("");
   const [location, setLocation] = useState("");
-  const onClickHandler = () => {
-    const contactData = { name, number: mobile, location };
-    props.onContactAdded(contactData);
+  const onFormSubmitHandler = (e) => {
+    e.preventDefault();
+
+    const contactData = {
+      id: Math.random(),
+      name,
+      number: mobile,
+      location,
+    };
+    console.log(typeof mobile);
+    if (
+      name.length < 1 ||
+      (mobile.length < 1 && typeof mobile !== Number) ||
+      location < 1
+    ) {
+      alert("Please fill all data correctly");
+    } else {
+      props.onContactAdded(contactData);
+      setName("");
+      setMobile("");
+      setLocation("");
+    }
   };
 
   // const saveData = () => {
@@ -30,27 +49,31 @@ const ContactAdder = (props) => {
   return (
     <>
       <div className="simpleAdder">
-        Contact Adder:
-        <input
-          type="text"
-          value={name}
-          placeholder="Contact Name"
-          onChange={(e) => setName(e.target.value)}
-        ></input>
-        <input
-          type="text"
-          value={mobile}
-          placeholder="Mobile"
-          onChange={(e) => setMobile(e.target.value)}
-        ></input>
-        <input
-          type="text"
-          value={location}
-          placeholder="Contact Location"
-          onChange={(e) => setLocation(e.target.value)}
-        ></input>
-        <br />
-        <button onClick={onClickHandler}>Click Me!</button>
+        <h3>Contact Adder:</h3>
+        <form onSubmit={onFormSubmitHandler}>
+          <br />
+          <input
+            type="text"
+            value={name}
+            placeholder="Contact Name"
+            onChange={(e) => setName(e.target.value)}
+          ></input>
+          <input
+            type="number"
+            value={mobile}
+            placeholder="Mobile"
+            onChange={(e) => setMobile(e.target.value)}
+          ></input>
+          <input
+            type="text"
+            value={location}
+            placeholder="Contact Location"
+            onChange={(e) => setLocation(e.target.value)}
+          ></input>
+          <br />
+          <button>Add Contacts</button>
+        </form>
+       
         {/* <button onClick={saveData}>Save data</button>
         <button onClick={getData}>Get data</button>
         <button onClick={clearData}>Clear data</button> */}
